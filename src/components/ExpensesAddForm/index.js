c40de06569cec7380dedcb266c574a5b5aab6711
Arthur Vincent Simon <developarvin@gmail.com>
2016-10-01 11:54:08 +0800

    Initial implementation of Expenses Add Form

---
+++ src/components/ExpensesAddForm/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {
+    Text,
+    View
+} from 'react-native'
+
+class ExpensesAddForm extends Component {
+    render() {
+        return (
+            <View>
+                <Text>ExpensesAddForm</Text>
+            </View>
+        )
+    }
+}
+
+Expenses.propTypes = {}
+Expenses.defaultProps = {}
+
+export default ExpensesAddForm
---
+++ src/containers/ExpensesAddScene.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { connect } from 'react-redux'
+
+import ExpensesHeader from '../components/ExpensesHeader'
+import ExpensesAddForm from '../components/ExpensesAddForm'
+
+class ExpensesAddScene extends Component {
+    render() {
+        return (
+            <View>
+                <ExpensesHeader />
+                <View>
+                    <ExpensesAddForm />
+                </View>
+            </View>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {}
+}
+
+function mapDispatchToProps(dispatch) {
+    return {}
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(ExpensesShowScene)
