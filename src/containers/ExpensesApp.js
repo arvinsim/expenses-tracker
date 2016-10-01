7f5af4e2955bd7b5065f4f00d7b924955e2b3545
Arthur Vincent Simon <developarvin@gmail.com>
2016-10-01 10:55:57 +0800

    Update ExpensesApp to use Navigator

--- src/containers/ExpensesApp.js
+++ src/containers/ExpensesApp.js
@@ -1,34 +1,20 @@
-/*
-import { Expenses } from './js/components/Expenses/index.js'
-import { ExpensesAdd } from './js/components/ExpensesAdd/index.js'
+import React, { Component } from 'react'
+import { View, Navigator } from 'react-native'
+import { connect } from 'react-redux';
 
-class ExpensesManager extends Component {
+import ShowExpensesScene from './ShowExpensesScene'
+
+class ExpensesApp extends Component {
     renderScene(route, navigator) {
         if (route.index === 0) {
             // Home
             return (
-                <Expenses
-                    onAddExpense={() => {
-                        const nextIndex = 1
-                        navigator.push({
-                            index: nextIndex
-                        })
-                    }}
-
-                    />
+                <View>
+                    <ShowExpensesScene />
+                </View>
             )
         } else if (route.index === 1) {
-            // Add Expense
-            return (
-                <ExpensesAdd
-                    // Function to call to go back to the previous scene
-                    onBack={() => {
-                        if (route.index > 0) {
-                            navigator.pop();
-                        }
-                    }}
-                    />
-            )
+            // TODO: Add Expense
         }
     }
 
@@ -37,24 +23,7 @@ class ExpensesManager extends Component {
             <Navigator
                 initialRoute={{ title: 'My Initial Scene', index: 0 }}
                 renderScene={ this.renderScene }
-                />
-        )
-    }
-}
-*/
-
-import React, { Component } from 'react'
-import { View } from 'react-native'
-import { connect } from 'react-redux';
-
-import ShowExpensesScene from './ShowExpensesScene'
-
-class ExpensesApp extends Component {
-    render() {
-        return (
-            <View>
-                <ShowExpensesScene />
-            </View>
+            />
         )
     }
 }
