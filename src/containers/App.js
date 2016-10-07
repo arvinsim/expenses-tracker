ca64cecfa51dd024d6573fa131e97d23ed3cd748
Arthur Vincent Simon <developarvin@gmail.com>
2016-10-08 06:45:16 +0800

    - Add immutable dependency - Remove redux-thunk dependency

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
 
 import expensesReducers from '../redux/modules/index'
 import ExpensesApp from './ExpensesApp'
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 const store = createStoreWithMiddleware(expensesReducers)
 
 export default class App extends Component {
